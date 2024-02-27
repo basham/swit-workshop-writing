6de80216826c202ad381421a5670344b525d7ee6
Chris Basham <chris@bash.am>
2024-02-27 11:58:48 -0500

    Update theme

--- slides.js
+++ slides.js
@@ -3,23 +3,23 @@ import Markdown from './reveal.js/plugin/markdown/markdown.esm.js';
 import Notes from './reveal.js/plugin/notes/notes.esm.js';
 
 Reveal.initialize({
-  backgroundTransition: 'none',
-  dependencies: [
-    {
-      async: true,
-      condition: () => !!document.body.classList,
-      src: 'reveal-a11y/accessibility/helper.js',
-    }
-  ],
-  hash: true,
-  markdown: {
-    smartypants: true
-  },
-  plugins: [
-    Markdown,
-    Notes
-  ],
-  transition: 'none'
+	backgroundTransition: 'none',
+	dependencies: [
+		{
+			async: true,
+			condition: () => !!document.body.classList,
+			src: 'reveal-a11y/accessibility/helper.js',
+		}
+	],
+	hash: true,
+	markdown: {
+		smartypants: true
+	},
+	plugins: [
+		Markdown,
+		Notes
+	],
+	transition: 'none'
 });
 
 Reveal.on('ready', updateTheme);
@@ -27,6 +27,6 @@ Reveal.on('ready', updateTheme);
 Reveal.on('slidechanged', updateTheme);
 
 function updateTheme (event) {
-  const isPrimary = !!event.currentSlide.querySelector('h1');
-  document.body.classList.toggle('reveal-viewport--primary', isPrimary);
+	const isPrimary = !!event.currentSlide.querySelector('h1');
+	document.body.classList.toggle('reveal-viewport--primary', isPrimary);
 }
